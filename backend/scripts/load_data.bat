8dabdbf4ebd3dff13b3004a4d3e3f56e5142c625
abhishiek <abhishiek.work@gmail.com>
2026-01-29 15:08:46 +0530

    test

--- backend/scripts/load_data.bat
+++ backend/scripts/load_data.bat
@@ -1,7 +1,6 @@
 @echo off
 echo Loading database content from JSON...
-REM Change directory to the 'backend' folder, relative to this script
-cd /d "%~dp0\.."
+cd /d "%~dp0.."
 python manage.py migrate
 python manage.py loaddata core/fixtures/core_data.json
 echo Done! Your database is now in sync.
